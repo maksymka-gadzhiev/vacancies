11d9b83f01fa65007d6213b1df78fd0398123700
maksymka-gadzhiev <MaKsy1M@yandex.ru>
2024-10-27 23:34:44 +0300

    Service

---
+++ Service/Program.cs
@@ -0,0 +1,15 @@
+using vacancies.Service.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+
+SerilogConfigurator.ConfigureService(builder);
+SwaggerConfigurator.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+SerilogConfigurator.ConfigureApplication(app);
+SwaggerConfigurator.ConfigureApplication(app);
+
+app.UseHttpsRedirection();
+
+app.Run();
