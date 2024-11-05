a6457dcbb77069e61a79e4edd2e6e8e42bb2806c
maksymka-gadzhiev <MaKsy1M@yandex.ru>
2024-11-05 21:48:00 +0300

    Update migration

--- Vacancies.DataAccess/Entities/BaseEntity.cs
+++ Vacancies.DataAccess/Entities/BaseEntity.cs
@@ -3,4 +3,8 @@
 public class BaseEntity
 {
     public int Id { get; set; }
+
+    public Guid ExternalId { get; set; }
+    public DateTime ModificationTime { get; set; }
+    public DateTime CreationTime { get; set; }
 }
---
+++ Vacancies.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Vacancies.DataAccess.Entities;
+
+namespace Vacancies.DataAccess.Repository;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    IQueryable<T> GetAll();
+    IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
+    T? GetById(int id);
+    T? GetById(Guid id);
+    T Save(T entity);
+    void Delete(T entity);
+}
