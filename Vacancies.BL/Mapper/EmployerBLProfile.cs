d8dcd21528749827db486c70b3d5a5dd42c40a29
maksymka-gadzhiev <MaKsy1M@yandex.ru>
2024-11-12 22:06:08 +0300

    Profile

---
+++ Vacancies.BL/Mapper/EmployerBLProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Vacancies.BL.Employers.Entity;
+using Vacancies.DataAccess.Entities;
+
+namespace Vacancies.BL.Mapper;
+
+public class EmployerBLProfile : Profile
+{
+    public EmployerBLProfile()
+    {
+        CreateMap<EmployerEntity, EmployerModel>()
+            .ForMember(x => x.Id, y => y.MapFrom(src => src.Id));
+        CreateMap<CreateEmployerModel, EmployerModel>()
+            .ForMember(x => x.Id, y => y.Ignore())
+            .ForMember(x => x.CreationTime, y => y.Ignore())
+            .ForMember(x => x.ModificationTime, y => y.Ignore());
+        
+        CreateMap<UpdateEmployerModel, EmployerModel>()
+            .ForMember(x => x.Id, y => y.MapFrom(src => src.Id))
+            .ForMember(x => x.ModificationTime, y => y.Ignore());
+    }
+}
